6dd25986803d50c44b9f0393df1fb9630dcdf411
nguyentheloc-310101 <nguyentheloc310101@gmail.com>
2023-05-07 12:48:11 +0700

    fix-promise

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -55,7 +55,7 @@ const Layout = ({ title, children }) => {
       </Head>
       <div className="w-full flex min-h-screen flex-col justify-between ">
         <header>
-          <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4">
+          <Navbar className="fixed inset-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4">
             <div className="flex items-center justify-between text-blue-gray-900">
               <Typography className="mr-4 cursor-pointer py-1.5 font-bold">
                 <Link href="/">Shopping</Link>
